2a6ee36912c85488218fdb1d4bc637c3c0fcceba
AdrianMilo <adrianestebanlescano@gmail.com>
2022-12-24 14:45:58 -0300

    agregando funciones a tabla

--- scripts/appsadmin.js
+++ scripts/appsadmin.js
@@ -1,6 +1,6 @@
-const gameSection = document.getElementById('GamesSection')
+const AdminJuegos = document.getElementById('AdminJuegos')
 
-let tableBody = document.getElementById('TableAdminBody')
+let tableBody = document.getElementById('AdminTableBody')
 console.log(tableBody)
 fetch('http://localhost:3000/games')
   .then(response => response.json())
@@ -9,19 +9,21 @@ fetch('http://localhost:3000/games')
       let gameRow = document.createElement('tr');
       gameRow.scope = 'row'
       gameRow.innerHTML = `
-        <td class='text-bold'>${item.orden}</td>
+        <td class='text-bold'>${item.id}</td>
         <td class='text-bold'>${item.title}</td>
         <td class='text-bold'>${item.short_description}</td>  
-        <td class='text-bold'>${genre}</td>
+        <td class='text-bold'>${item.genre}</td>
         <td class='text-bold'>
-          <i id='${item.id}' class ="bi bi-trash" onclick="deleteGame(this)"></i>
+         <i id='${item.id}' class ="bi bi-trash" onclick="deleteGame(this)"></i>
+         <i id='${item.id}' class="bi bi-pencil-fill" onclick="editGame(this)"></i>
+         <i id='${item.id}' class="bi bi-star"></i>
         </td>
       `
       tableBody.appendChild(gameRow)
     });
   });
   // function deleteGame(element) {
-  //   const isDelete = confirm('¿Deseas eliminar el elemento?')  
+  //   const isDelete = confirm('¿Deseas eliminar el juego?')  
   //   if (isDelete){
   //     fetch(`http://localhost:3000/games${element.id}`, {
   //       method: 'DELETE'
@@ -30,5 +32,8 @@ fetch('http://localhost:3000/games')
   //     .then( () => { window.location.reload() })
   //   }    
   // }
+
+
+  
 
   
